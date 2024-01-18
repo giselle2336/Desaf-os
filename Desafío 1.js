79f698146c7e355f82e31652a2c8eaccd5ccb021
giselle2336 <39502897+giselle2336@users.noreply.github.com>
2024-01-18 12:56:56 -0600

    Add files via upload

---
+++ Desafío 1.js
@@ -0,0 +1,13 @@
+alert ('¡Bienvenida y bienvenido a nuestro sitio web!');
+let nombre = Luna;
+let edad = 25;
+let numeroDeVentas = 50;
+let saldoDisponible = 1000;
+alert ('¡Error! Completa todos los campos');
+let mensajeDeError = ('¡Error! Completa todos los campos');
+alert (mensajeDeError);
+let nombre = prompt ('Escribe tu nombre de usuario');
+let edad = prompt ('Ingresa tu edad');
+if (edad> = 18) {
+    alert ('¡Puedes obtener tu licencia de conducir!');
+}
